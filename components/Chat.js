3ba120f7751f32a001f5f36ddefef22431ae72bd
Hadeon <andersene909@gmail.com>
2018-10-29 13:51:01 -0700

    Connecting chat with firebase;

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import Fire from '../Fire';
 
 class Chat extends Component {
 
@@ -12,10 +13,31 @@ class Chat extends Component {
     messages: []
   };
 
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
+  }
+
+  get user() {
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Fire.shared.uid,
+    };
+  }
+
+  componentWillUnmount() {
+    Fire.shared.off();
+  }
+
   render() {
     return (
       <GiftedChat
         messages={this.state.messages}
+        onSend={Fire.shared.send}
+        user={this.user}
       />
     );
   }
